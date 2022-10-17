130a869e5380769244ee441774945629697b00f4
Vishnu Vardhan <vishnuthecoder@gmail.com>
2022-10-17 18:03:50 +0530

    Added my mergeSortedArray method

--- Sorting/MergeSort.js
+++ Sorting/MergeSort.js
@@ -1,5 +1,20 @@
-function mergeSort() {
+function mergeSort() {}
 
+function mergeSortedArrays(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  let result = [];
+
+  for (let index = 0; index < arr1.length + arr2.length; index++) {
+    if (arr1[i] < arr2[j] && i < arr1.length) {
+      result.push(arr1[i]);
+      i++;
+    } else if (arr1[i] > arr2[j] && j < arr2.length) {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  return result;
 }
 
-console.log(mergeSort[]);
+console.log(mergeSortedArrays([1, 10, 12, 34], [2, 8, 11, 22, 31]));
